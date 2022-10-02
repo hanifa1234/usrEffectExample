8904bc35f9ecc43cd8b061037966db48aa37f2df
Mohammad: Abu Hanif <80195778+hanifa1234@users.noreply.github.com>
2022-10-02 12:20:37 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseEffectExample from "./COMPONET/HOOKS/useEffectExample.jsx"
+
+function App() {
+  return (
+    <div>
+      <UseEffectExample />
+    </div>
+  )
+}
+
+export default App
---
+++ src/COMPONET/HOOKS/useEffectExample.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+
+function useEffectExample() {
+
+    const [count, setCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    useEffect(() => {
+        console.log("useEffect");
+    }, [count])
+
+    return (
+        <div>
+            {
+                console.log("randaring")
+            }
+            <h1> Count : {count} </h1>
+
+
+            <button onClick={() => {
+                setCount(count => count + 1)
+            }} >
+                +
+            </button>
+
+            <button onClick={() => {
+                setIsLoading(!isLoading);
+            }} >
+                isLoading
+            </button>
+
+        </div>
+
+    )
+}
+
+export default useEffectExample
